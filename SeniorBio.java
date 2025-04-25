bc740dd742bcb925b5c4fc1b1acf6a1522e05599
OrcaCS <stephanie.hu2@student.tdsb.on.ca>
2025-04-24 22:54:38 -0400

    Update SeniorBio.java

--- SeniorBio.java
+++ SeniorBio.java
@@ -1,5 +1,40 @@
 public class SeniorBio extends JuniorScience {
     protected int bioGrade;
+    protected int predictionDifference = 0;
 
-    protected int // projected/predicted bio grade from junior science
+    protected SeniorBio() {
+        super(100);
+        this.bioGrade = 100;
+    }
+
+    public SeniorBio(int grade) {
+        super(grade);
+        this.bioGrade = 100;
+    }
+
+    protected int getBioGrade() {
+        return bioGrade;
+    }
+
+    protected int getPrediction() {
+        return predictionDifference;
+    }
+
+    protected void setBioGrade(int bioGrade) {
+        this.bioGrade = bioGrade;
+    }
+
+    protected void setPrediction(int predictionDifference) {
+        this.predictionDifference = predictionDifference;
+    }
+
+    protected int predictedBioGradeMidterm() { // projected/predicted bio grade from junior science
+        return this.bioGrade + this.predictionDifference;
+    }
+
+    protected double predictedBioGrade() { // projected/predicted bio grade from junior science
+        double weightedBioGrade = super.calculateWeightedBioGrade(this.grade, this.weightPercentBio);
+        double actualBioGrade = super.calculateActualBioGrade(weightedBioGrade, this.weightPercentBio);
+        return actualBioGrade + this.predictionDifference;
+    }
 }
