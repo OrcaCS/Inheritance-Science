fefcbe64ea6ab71292eb42f2f633f18593a6bed6
OrcaCS <stephanie.hu2@student.tdsb.on.ca>
2025-04-28 21:28:34 -0400

    Update SeniorBio.java

--- SeniorBio.java
+++ SeniorBio.java
@@ -8,40 +8,47 @@ public class SeniorBio extends JuniorScience {
     /** Grade in biology. */
     protected int bioGrade;
 
-    /** Predicted difference between junior biology grade and biology course. */
+    /** Predicted difference between junior science biology unit grade and biology course. */
     protected int predictionDifference = 0;
 
-    /** Default constructor. */
+    /** Constructor for perfect grades. */
     protected SeniorBio() {
         super(100); // default science grade is 100
         this.bioGrade = 100;
     }
 
+    /** Constructor for chosen junior science grade and default 100 for biology grade. */
     public SeniorBio(int grade) {
         super(grade);
         this.bioGrade = 100;
     }
 
+    /** Gets biology grade. */
     protected int getBioGrade() {
         return bioGrade;
     }
 
+    /** Gets prediction difference. */
     protected int getPrediction() {
         return predictionDifference;
     }
 
+    /** Sets biology grade. */
     protected void setBioGrade(int bioGrade) {
         this.bioGrade = bioGrade;
     }
 
+    /** Sets prediction difference. */
     protected void setPrediction(int predictionDifference) {
         this.predictionDifference = predictionDifference;
     }
 
-    protected int predictedBioGradeMidterm() { // projected/predicted bio grade from junior science
+    /** Predicts biology course grade using biology midterm marks. */
+    protected int predictedBioGradeMidterm() { // projected/predicted bio grade from midterm
         return this.bioGrade + this.predictionDifference;
     }
 
+    /** Predicts biology course grade using junior science biology unit grade. */
     protected double predictedBioGrade() { // projected/predicted bio grade from junior science
         double weightedBioGrade = super.calculateWeightedBioGrade(grade, weightPercentBio);
         double actualBioGrade = super.calculateActualBioGrade(weightedBioGrade, weightPercentBio);
