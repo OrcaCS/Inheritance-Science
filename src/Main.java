1595a4743281389941563160101cf10592d58d0f
OrcaCS <stephanie.hu2@student.tdsb.on.ca>
2025-04-30 00:18:38 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         SeniorBio sr = new SeniorBio(70);
+        UniversityPhys uni = new UniversityPhys();
 
         // sr.setWeightPercentBio(50);
         // sr.setPrediction(10);
 
         // System.out.println("\nPredicted: " + sr.predictedBioGrade());
 
-
         // System.out.println("\nexam grade needed: " + sr.examGradeNeeded(91, 30, 93));
-        System.out.println("\nfinal grade: " + sr.finalGrade(100, 30, 98));
+        // System.out.println("\nfinal grade: " + sr.finalGrade(100, 30, 98));
+
+        uni.uWaterlooPhysicsRequirements();
     }
 }
