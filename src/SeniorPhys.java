d673a2f77c30d219e2f37574e3ec7303383ca52a
OrcaCS <stephanie.hu2@student.tdsb.on.ca>
2025-04-29 23:36:56 -0400

    Update SeniorPhys.java

--- src/SeniorPhys.java
+++ src/SeniorPhys.java
@@ -8,7 +8,10 @@ public class SeniorPhys extends JuniorScience {
     /** Grade in physics. */
     protected int physGrade;
 
-    /** Predicted difference between junior science physics unit grade and physics course. */
+    /**
+     * Predicted difference between junior science physics unit grade and physics
+     * course.
+     */
     protected int predictionDifference = 0;
 
     /** Constructor for perfect grades. */
@@ -17,57 +20,101 @@ public class SeniorPhys extends JuniorScience {
         this.physGrade = 100;
     }
 
-    /** Constructor for chosen junior science grade and default 100 for physics grade. */
+    /**
+     * Constructor for chosen junior science grade and default 100 for physics
+     * grade.
+     */
     public SeniorPhys(int grade) {
         super(grade);
         this.physGrade = 100;
     }
 
-    /** Constructor for chosen junior science grade and default 100 for physics grade. */
+    /**
+     * Constructor for chosen junior science grade and default 100 for physics
+     * grade.
+     */
     public SeniorPhys(int grade, int physGrade) {
         super(grade);
         this.physGrade = physGrade;
     }
 
-    /** Gets physics grade. */
+    /**
+     * Gets physics grade.
+     * 
+     * @return This method returns the physics grade.
+     */
     protected int getPhysGrade() {
         return physGrade;
     }
 
-    /** Gets prediction difference. */
+    /**
+     * Gets prediction difference.
+     * 
+     * @return This method returns the predicted difference between junior
+     *         science biology unit grade and biology course.
+     */
     protected int getPrediction() {
         return predictionDifference;
     }
 
-    /** Sets physics grade. */
+    /**
+     * Sets physics grade.
+     * 
+     * @param physGrade This is the physics grade.
+     */
     protected void setPhysGrade(int physGrade) {
         this.physGrade = physGrade;
     }
 
-    /** Sets prediction difference. */
+    /**
+     * Sets prediction difference.
+     * 
+     * @param predictionDifference This is the predicted difference between junior
+     *                             science biology unit grade and biology course.
+     */
     protected void setPrediction(int predictionDifference) {
         this.predictionDifference = predictionDifference;
     }
 
-    /** Predicts physics course grade using physics midterm marks. */
+    /** Predicts physics course grade using physics midterm marks.
+     * 
+     * @return This is the predicted physics grade.
+     */
     protected int predictedPhysGradeMidterm() { // projected/predicted phys grade from midterm
         return this.physGrade + this.predictionDifference;
     }
 
-    /** Predicts physics course grade using junior science physics unit grade. */
+    /** Predicts physics course grade using junior science physics unit grade.
+     * 
+     * @return This is the predicted physics grade.
+     */
     protected double predictedPhysGrade() { // projected/predicted phys grade from junior science
         double actualPhysGrade = super.calculateActualPhysGrade();
         return actualPhysGrade + predictionDifference;
     }
 
-    /** Calculates the physics exam grade required for a final grade goal. */
+    /**
+     * Calculates the physics exam grade required for a final grade goal.
+     * 
+     * @param physGrade         This is the grade in physics.
+     * @param examWeightPercent This is the weight of the exam in percent.
+     * @param finalGoal         This is the user-inputted grade goal.
+     * @return This method returns the exam grade needed to reach the grade goal.
+     */
     protected double examGradeNeeded(int physGrade, double examWeightPercent, int finalGoal) {
         double classWeightPercent = 100 - examWeightPercent;
         double weightedPhysGrade = physGrade * (classWeightPercent / 100);
         return (finalGoal - weightedPhysGrade) / (examWeightPercent / 100);
     }
 
-    /** Calculates the final physics grade using the grade before exam and hypothetical exam grade. */
+    /**
+     * Calculates the final grade from grade before exam and theoretical exam grade.
+     * 
+     * @param physGrade         This is the grade in physics.
+     * @param examWeightPercent This is the weight of the exam in percent.
+     * @param examGrade         This is the exam grade.
+     * @return This method returns the final grade.
+     */
     protected double finalGrade(int physGrade, double examWeightPercent, double examGrade) {
         double classWeightPercent = 100 - examWeightPercent;
         double weightedPhysGrade = physGrade * (classWeightPercent / 100);
